746f86c58b84d6082bd25e78cb3b0c0f02edf49e
MarcocraMarco <111215085+MarcocraMarco@users.noreply.github.com>
2025-04-13 10:29:01 +0200

    Delete Ex_2 directory

--- Ex_2/pdfTextExtraction.py
+++
@@ -1,8 +0,0 @@
-from PyPDF2 import PdfReader
-reader = PdfReader("2023_SAS_Textbook_CT_chap_10_shortened_v4.pdf")
-
-num_pagine = len(reader.pages)
-print(f"Numero di pagine: {num_pagine}")
-
-for page in reader.pages:
-    print(page.extract_text())
