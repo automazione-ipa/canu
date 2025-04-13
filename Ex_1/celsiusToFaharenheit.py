fa1bcd77f91c81d0c5438fde04676a678053a742
MarcocraMarco <111215085+MarcocraMarco@users.noreply.github.com>
2025-04-13 10:29:15 +0200

    Delete Ex_1 directory

--- Ex_1/celsiusToFaharenheit.py
+++
@@ -1,6 +0,0 @@
-celsius=input("Insert temperature in celsius to be converted\n")
-def converter(var):
-    fahrenheit=1.8*float(var)+32
-    return fahrenheit
-
-print("Celsius "+ celsius +" ---> Farhenight ",round(converter(celsius), 2))
